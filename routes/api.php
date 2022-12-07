c612416666c2b77d0333fcc77fd25b9a608ae733
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-12-06 21:31:40 -0400

    feat(auth): add login logic

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('sanctum/token', 'UserTokenController');
 
 Route::apiResource('products', 'ProductController')->middleware('auth:sanctum');
 Route::apiResource('categories', 'CategoryController')->middleware('auth:sanctum');
