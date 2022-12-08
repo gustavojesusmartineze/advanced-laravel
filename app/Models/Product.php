0bb09e1a78d96fdfa46514d22694cf3e10457a12
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-12-08 07:47:18 -0400

    feat(products): add relation between products and users

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,7 +13,11 @@ class Product extends Model
 
     // protected $fillable = [];
 
-    function category() {
+    public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function createdBy() {
+        return $this->belongsTo(User::class);
     }
 }
