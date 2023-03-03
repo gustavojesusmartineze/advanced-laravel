54cd298e4aafaade8c5d2011109159001146a2fe
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-03-03 13:01:15 -0400

    feat(queues): add queued job for Send Welcome Email

---
+++ app/Jobs/SendWelcomeEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\WelcomeEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $userEmail;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(string $userEmail)
+    {
+
+        $this->userEmail = $userEmail;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $mail = new WelcomeEmail();
+
+        Mail::to($this->userEmail)->send($mail);
+    }
+}
